a4c32c6c19af2269641f6cec647e1706e9c1a7cb
Alex Costa <alex@deanx.com.br>
2016-05-17 03:42:10 -0300

    third and last method

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 'use strict'
+
 const dateOne = process.argv[2];
 const dateTwo = process.argv[3];
 
 const separator = '-';
 const daysInMonths = [null, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-let isTheSameYearAndMonth = (dateOne, dateTwo)  =>  dateOne.substr(0,7) === dateTwo.substr(0,7);
-let isTheSameYear = (dateOne, dateTwo) => dateOne.substr(0,4) === dateTwo.substr(0,4);
+const isTheSameYearAndMonth = (dateOne, dateTwo)  =>  dateOne.substr(0,7) === dateTwo.substr(0,7);
+const isTheSameYear = (dateOne, dateTwo) => dateOne.substr(0,4) === dateTwo.substr(0,4);
 
-let isSpecialYear = (year) => {
+const isSpecialYear = (year) => {
   return (year % 4 === 0 && year % 100 !== 0 ) || (year % 400 === 0);
 };
 
 
-let parseDate = (date) => {
+const parseDate = (date) => {
     let splittedDate = date.split(separator);
     return {
       year: parseInt(splittedDate[0]),
@@ -22,7 +23,7 @@ let parseDate = (date) => {
     };
 };
 
-let daysUntilEndOfMonth = (date) => {
+const daysUntilEndOfMonth = (date) => {
   let parsedDate = parseDate(date);
   let lastDay = daysInMonths[parsedDate.month];
   if(parsedDate.month === 2 && isSpecialYear(parsedDate.year)) {
@@ -32,32 +33,32 @@ let daysUntilEndOfMonth = (date) => {
   return Math.max(0, lastDay - parsedDate.day);
 }
 
-let calculateDaysBetweenMonths = (monthStart, monthEnd, year) => {
+const calculateDaysBetweenMonths = (monthStart, monthEnd, year) => {
   let totalDays = 0;
   for(let i = monthStart + 1; i < monthEnd; i++) {
     totalDays += daysInMonths[i];
   }
 
-  if(isSpecialYear(year)) {
+  if(isSpecialYear(year) && monthStart <= 2) {
     totalDays++;
   }
   return totalDays;
 
 };
 
-let calculateBiggerDate = (dateOne, dateTwo) => {
+const calculateBiggerDate = (dateOne, dateTwo) => {
     return {
       endDate: dateOne > dateTwo ? dateOne : dateTwo,
       startDate: dateTwo > dateOne ? dateOne : dateTwo
     };
 };
 
-let calculateDateFromDifferentMonths = (dateOne, dateTwo) => {
+const calculateDateFromDifferentMonths = (dateOne, dateTwo) => {
   let totalDays = 0;
   const orderedDates = calculateBiggerDate(dateOne, dateTwo);
 
   let parsedStartDate = parseDate(orderedDates.startDate);
-  let parsedEndDate = parseDate(orderedDates.endDate );
+  let parsedEndDate = parseDate(orderedDates.endDate);
 
   totalDays = calculateDaysBetweenMonths(parsedStartDate.month, parsedEndDate.month, parsedEndDate.year);
 
@@ -65,17 +66,38 @@ let calculateDateFromDifferentMonths = (dateOne, dateTwo) => {
 
   totalDays += parsedEndDate.day;
   return totalDays;
-}
+};
 
 
-let calculateDateFromDifferentYearAndMonth = (dateOne, dateTwo) => {
+const calculateDateFromDifferentYearAndMonth = (dateOne, dateTwo) => {
 
   const calculateDaysFromYears = (yearStart, yearEnd) => {
     let totalDays = 0;
     for(let i = yearStart + 1; i < yearEnd; i++) {
       isSpecialYear(i) ? totalDays += 366 : totalDays += 365;
     }
+    return totalDays;
+  };
 
+  const calculateUntilEndOfYear = (date) => {
+    let totalDays = 0;
+    let parsedDate = parseDate(date);
+
+    totalDays += daysUntilEndOfMonth(date);
+
+    totalDays += calculateDaysBetweenMonths(parsedDate.month, 12, parsedDate.year);
+    if(parsedDate.month < 12) totalDays += 31;
+    return totalDays;
+  };
+
+  const calculateFromStartOfYear = (date) => {
+    let parsedDate = parseDate(date);
+    let totalDays = parsedDate.day;
+
+    for(let i = 0; i < parsedDate.month; i++) {
+      totalDays += daysInMonths[i];
+    }
+    if(isSpecialYear(parsedDate.year) && parsedDate.month > 2) totalDays++;
     return totalDays;
   };
 
@@ -83,12 +105,14 @@ let calculateDateFromDifferentYearAndMonth = (dateOne, dateTwo) => {
 
   let parsedStartDate = parseDate(orderedDates.startDate);
   let parsedEndDate = parseDate(orderedDates.endDate);
-
-  return calculateDaysFromYears(parsedStartDate.year, parsedEndDate.year);
+  let totalDays = calculateUntilEndOfYear(orderedDates.startDate)
+    + calculateDaysFromYears(parsedStartDate.year, parsedEndDate.year)
+    + calculateFromStartOfYear(orderedDates.endDate);
+    return totalDays;
 };
 
-let calculateDateFromSameYearAndMonth = (dateOne, dateTwo) => {
-  let difference = (parseInt(dateOne.split(separator).join('')) - parseInt(dateTwo.split(separator).join(''))) -1;
+const calculateDateFromSameYearAndMonth = (dateOne, dateTwo) => {
+  let difference = (parseInt(dateOne.split(separator).join('')) - parseInt(dateTwo.split(separator).join(''))) ;
   return difference > 0 ? difference : difference * -1;
 };
 
@@ -100,9 +124,11 @@ const calculate = (dateOne, dateTwo) => {
     totalDays = calculateDateFromSameYearAndMonth(dateOne, dateTwo);
   } else if(isTheSameYear(dateOne, dateTwo)){
     totalDays = calculateDateFromDifferentMonths(dateOne, dateTwo);
+  } else {
+    totalDays = calculateDateFromDifferentYearAndMonth(dateOne, dateTwo);
   }
 
-  return totalDays -2;  //drop start and end dates
+  return totalDays -1;  //drop start and end dates
 }
 
 console.log(calculate(dateOne, dateTwo));
